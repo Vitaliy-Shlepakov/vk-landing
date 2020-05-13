6a26092b66a8bb709829c4c2fdf386691b3a5258
Vik <shlepakov58@gmail.com>
2020-05-13 10:58:32 +0300

    style fix

--- src/components/FixedArea/FixedArea.jsx
+++ src/components/FixedArea/FixedArea.jsx
@@ -15,7 +15,8 @@ const FixedArea = () => {
                     ВКонтакте всегда с вами
                 </h1>
                 <p className="fixedArea__description">
-                    Установите на телефон официальное приложение ВКонтакте и следите за важными событиями из жизни близких, где бы Вы не находились!
+                    {/*Установите на телефон официальное приложение ВКонтакте и следите за важными событиями из жизни близких, где бы Вы не находились!*/}
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur deserunt ea eos incidunt labore minima mollitia, quos repellendus saepe voluptatibus?
                 </p>
                 <div className="fixedArea__actions">
                     <div className="fixedArea__action">
@@ -33,17 +34,17 @@ const FixedArea = () => {
                 </div>
                 <div className="fixedArea__qrCode">
                     <QrCode/>
-                    <p className="fixed__proposal">
+                    <p className="fixedArea__proposal">
                         или&nbsp;
-                        <a href="#" className="fixed__sms-link">
+                        <a href="#" className="fixedArea__sms-link">
                             получить ссылку сообщением на телефон
                         </a>
                     </p>
                 </div>
             </div>
             <div className="fixedArea__footer">
-                &copy;2020&nbsp;
-                <a href="#" className="fixedArea__vk">Команда Вконтакте</a>
+                &copy;&nbsp;2020&nbsp;
+                <a href="#" className="fixedArea__vk">Команда</a>
             </div>
         </div>
     );
